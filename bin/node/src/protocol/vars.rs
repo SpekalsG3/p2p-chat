be0f9719107381a7a8bba71334aa6039bfb7ee7f
SpekalsG3 <karpick.kirill@gmail.com>
2024-03-02 02:35:56 +0300

    remove done todo

--- bin/node/src/protocol/vars.rs
+++ bin/node/src/protocol/vars.rs
@@ -3,7 +3,7 @@ use anyhow::{bail, Result};
 
 pub const PROT_OPCODE_CONTINUATION: u8 = 0b0000; // received frame is an continuation of previous unfinished frame
 pub const PROT_OPCODE_CONN_CLOSED:  u8 = 0b0001; // party disconnected // todo: send in case of graceful shutdown
-pub const PROT_OPCODE_PING:         u8 = 0b0010; // checking if connection is still alive // todo: send for fun
+pub const PROT_OPCODE_PING:         u8 = 0b0010; // checking if connection is still alive
 pub const PROT_OPCODE_PONG:         u8 = 0b0011; // answer if connection is still alive
 pub const PROT_OPCODE_DATA:         u8 = 0b0100; // frame contains application data
 pub const PROT_OPCODE_UPD_TOPOLOGY: u8 = 0b0101; // request to connect/reconnect/disconnect to provided IPs
