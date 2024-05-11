7872c90124414750e96d7a7e2fc51ee14620b804
SpekalsG3 <karpick.kirill@gmail.com>
2024-05-11 17:33:13 +0300

    remove unused code

--- examples/testing/src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let mut buf = "Hello world".as_bytes().to_vec();
-    buf.extend(vec![0; 256-buf.len()]);
-
-    let str = String::from_utf8_lossy(&buf);
-    println!("buf '{}'", str);
-}
